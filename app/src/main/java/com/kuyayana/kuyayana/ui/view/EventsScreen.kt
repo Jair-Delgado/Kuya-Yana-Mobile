e6a2cbff54a301a65850d778c2ea111a45b7c194
MisakiSan3 <iot.gomezyavirac.edu.ec>
2024-08-06 07:32:40 -0500

    Añadir column en eventScreen para poder escrolear hacia abajo. Isak

--- app/src/main/java/com/kuyayana/kuyayana/ui/view/EventsScreen.kt
+++ app/src/main/java/com/kuyayana/kuyayana/ui/view/EventsScreen.kt
@@ -7,8 +7,11 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material3.Button
@@ -80,15 +83,20 @@ fun EventsScreen (
     val month = calendar.get(Calendar.MONTH)
     val day = calendar.get(Calendar.DAY_OF_MONTH)
 
-    val datePickerDialog = remember {
-        DatePickerDialog(
-            context,
-            { _, year, month, dayOfMonth ->
-                startDate = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(
-                    Calendar.getInstance().apply {
-                        set(year, month, dayOfMonth)
-                    }.time
-                )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+    ){
+        val datePickerDialog = remember {
+            DatePickerDialog(
+                context,
+                { _, year, month, dayOfMonth ->
+                    startDate = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(
+                        Calendar.getInstance().apply {
+                            set(year, month, dayOfMonth)
+                        }.time
+                    )
                     TimePickerDialog(
                         context,
                         { _, hourOfDay, minute ->
@@ -99,21 +107,21 @@ fun EventsScreen (
                         startMinute,
                         true // Formato de 24 horas
                     ).show()
-            },
-            Calendar.getInstance().get(Calendar.YEAR),
-            Calendar.getInstance().get(Calendar.MONTH),
-            Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
-        )
-    }
-    val endDatePickerDialog = remember {
-        DatePickerDialog(
-            context,
-            { _, year, month, dayOfMonth ->
-                endDate = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(
-                    Calendar.getInstance().apply {
-                        set(year, month, dayOfMonth)
-                    }.time
-                )
+                },
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+            )
+        }
+        val endDatePickerDialog = remember {
+            DatePickerDialog(
+                context,
+                { _, year, month, dayOfMonth ->
+                    endDate = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(
+                        Calendar.getInstance().apply {
+                            set(year, month, dayOfMonth)
+                        }.time
+                    )
                     TimePickerDialog(
                         context,
                         { _, hourOfDay, minute ->
@@ -124,188 +132,191 @@ fun EventsScreen (
                         endMinute,
                         true // Formato de 24 horas
                     ).show()
-            },
-            Calendar.getInstance().get(Calendar.YEAR),
-            Calendar.getInstance().get(Calendar.MONTH),
-            Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
-        )
-    }
-    Column(
-        modifier = Modifier.padding(16.dp)
-    ) {
-        OutlinedTextField(
-            value = title,
-            onValueChange = { title = it },
-            label = { Text("Titulo") },
-            modifier = Modifier.fillMaxWidth()
-        )
-        Spacer(Modifier.padding(vertical = 8.dp))
-        OutlinedTextField(
-            value = description,
-            onValueChange = { description = it },
-            label = { Text("Descripccion") },
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Spacer(Modifier.padding(vertical = 8.dp))
-        OutlinedTextField(
-            value = "$startDate ${String.format("%02d:%02d", startHour, startMinute)}",
-            onValueChange = {},
-            label = { Text("Empieza") },
-            modifier = Modifier.fillMaxWidth(),
-            trailingIcon = {
-                IconButton(onClick = {
-                    datePickerDialog.show()
-                    //startTimePickerDialog.show()
-                }) {
-                    Icon(Icons.Filled.DateRange, contentDescription = "")
-                }
-            },
-            readOnly = true
-        )
-        Spacer(Modifier.padding(vertical = 8.dp))
-        OutlinedTextField(
-            value = "$endDate ${String.format("%02d:%02d", endHour, endMinute)}",
-            onValueChange = {},
-            label = { Text("Finaliza") },
-            modifier = Modifier.fillMaxWidth(),
-            trailingIcon = {
-                IconButton(onClick = {
-                    endDatePickerDialog.show()
-                   // endTimePickerDialog.show()
-                }) {
-                    Icon(Icons.Filled.DateRange, contentDescription = "")
-                }
-            },
-            readOnly = true
-        )
-
-        //CATEGORIA
-        Spacer(Modifier.padding(vertical = 8.dp))
-
-        var expandedCategory by remember { mutableStateOf(false) }
-        Box {
-            OutlinedTextField(
-                value = selectedCategory?.categoryName ?: "Seleccionar Categoria",
-                onValueChange = {},
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .clickable { expandedCategory = true },
-                enabled = false,
-                readOnly = true,
-                label = { Text("Categoria") }
+                },
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
             )
-            DropdownMenu(
-                expanded = expandedCategory,
-                onDismissRequest = { expandedCategory = false },
-
-                ) {
-                categories.forEach { category ->
-                    DropdownMenuItem(
-                        text = {Text(category.categoryName)},
-                        onClick = {
-                            selectedCategory = category
-                            expandedCategory = false
-                        }
-                    )
-                }
-            }
         }
-
-        //FINALIZA CATEGORIA-------------
-
-        Spacer(Modifier.padding(vertical = 8.dp))
-
-        var expanded by remember { mutableStateOf(false) }
-        Box {
-            OutlinedTextField(
-                value = selectedSubject?.subjectName ?: "Seleccionar Asignatura",
-                onValueChange = {},
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .clickable { expanded = true },
-                enabled = false,
-                readOnly = true,
-                label = { Text("Asignatura") }
-            )
-            DropdownMenu(
-                expanded = expanded,
-                onDismissRequest = { expanded = false },
-
-                ) {
-                subjects.forEach { subject ->
-                    DropdownMenuItem(
-                        text = {Text(subject.subjectName)},
-                        onClick = {
-                            selectedSubject = subject
-                            expanded = false
-                        }
-                    )
-                }
-            }
-        }
-        Spacer(Modifier.padding(vertical = 8.dp))
-        var expandedTeacher by remember { mutableStateOf(false)}
-        Box {
-            OutlinedTextField(
-                value = selectedTeacher?.teacherName?: "Seleccionar Profesor",
-                onValueChange = {},
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .clickable { expandedTeacher = true },
-                enabled = false,
-                readOnly = true,
-                label = { Text("Profesor") }
-            )
-            DropdownMenu(
-                expanded = expandedTeacher,
-                onDismissRequest = { expandedTeacher = false },
-                ) {
-                teachers.forEach { teacher ->
-                    DropdownMenuItem(
-                        text = {Text(teacher.teacherName)},
-                        onClick = {
-                            selectedTeacher = teacher
-                            expanded = false
-                        }
-                    )
-                }
-            }
-        }
-
-        Spacer(Modifier.padding(vertical = 16.dp))
-        Button(
-            onClick = {
-                if (selectedSubject != null ) {
-                    val newEvent = Event(
-                        title = title,
-                        description = description,
-                        start = "${startDate}T${String.format("%02d:%02d", startHour, startMinute)}-05:00",
-                        //start = start,
-                       // end = end,
-                        end ="${endDate}T${String.format("%02d:%02d", endHour, endMinute)}-05:00",
-                        teacher = teacher
-                    )
-                    eventViewModel.createEvent(
-                        newEvent,
-                        selectedSubject!!,
-                        selectedTeacher!!,
-                        selectedCategory!!
-                    )
-                    title = ""
-                    description = ""
-                   // end = ""
-                    endHour = 0
-                    endMinute = 0
-                    start = ""
-                }
-
-            },
-            modifier = Modifier.fillMaxWidth()
+        Column(
+            modifier = Modifier.padding(16.dp)
         ) {
-            Text("Crear Evento")
+            OutlinedTextField(
+                value = title,
+                onValueChange = { title = it },
+                label = { Text("Titulo") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(Modifier.padding(vertical = 8.dp))
+            OutlinedTextField(
+                value = description,
+                onValueChange = { description = it },
+                label = { Text("Descripccion") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(Modifier.padding(vertical = 8.dp))
+            OutlinedTextField(
+                value = "$startDate ${String.format("%02d:%02d", startHour, startMinute)}",
+                onValueChange = {},
+                label = { Text("Empieza") },
+                modifier = Modifier.fillMaxWidth(),
+                trailingIcon = {
+                    IconButton(onClick = {
+                        datePickerDialog.show()
+                        //startTimePickerDialog.show()
+                    }) {
+                        Icon(Icons.Filled.DateRange, contentDescription = "")
+                    }
+                },
+                readOnly = true
+            )
+            Spacer(Modifier.padding(vertical = 8.dp))
+            OutlinedTextField(
+                value = "$endDate ${String.format("%02d:%02d", endHour, endMinute)}",
+                onValueChange = {},
+                label = { Text("Finaliza") },
+                modifier = Modifier.fillMaxWidth(),
+                trailingIcon = {
+                    IconButton(onClick = {
+                        endDatePickerDialog.show()
+                        // endTimePickerDialog.show()
+                    }) {
+                        Icon(Icons.Filled.DateRange, contentDescription = "")
+                    }
+                },
+                readOnly = true
+            )
+
+            //CATEGORIA
+            Spacer(Modifier.padding(vertical = 8.dp))
+
+            var expandedCategory by remember { mutableStateOf(false) }
+            Box {
+                OutlinedTextField(
+                    value = selectedCategory?.categoryName ?: "Seleccionar Categoria",
+                    onValueChange = {},
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable { expandedCategory = true },
+                    enabled = false,
+                    readOnly = true,
+                    label = { Text("Categoria") }
+                )
+                DropdownMenu(
+                    expanded = expandedCategory,
+                    onDismissRequest = { expandedCategory = false },
+
+                    ) {
+                    categories.forEach { category ->
+                        DropdownMenuItem(
+                            text = {Text(category.categoryName)},
+                            onClick = {
+                                selectedCategory = category
+                                expandedCategory = false
+                            }
+                        )
+                    }
+                }
+            }
+
+            //FINALIZA CATEGORIA-------------
+
+            Spacer(Modifier.padding(vertical = 8.dp))
+
+            var expanded by remember { mutableStateOf(false) }
+            Box {
+                OutlinedTextField(
+                    value = selectedSubject?.subjectName ?: "Seleccionar Asignatura",
+                    onValueChange = {},
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable { expanded = true },
+                    enabled = false,
+                    readOnly = true,
+                    label = { Text("Asignatura") }
+                )
+                DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false },
+
+                    ) {
+                    subjects.forEach { subject ->
+                        DropdownMenuItem(
+                            text = {Text(subject.subjectName)},
+                            onClick = {
+                                selectedSubject = subject
+                                expanded = false
+                            }
+                        )
+                    }
+                }
+            }
+            Spacer(Modifier.padding(vertical = 8.dp))
+            var expandedTeacher by remember { mutableStateOf(false)}
+            Box {
+                OutlinedTextField(
+                    value = selectedTeacher?.teacherName?: "Seleccionar Profesor",
+                    onValueChange = {},
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable { expandedTeacher = true },
+                    enabled = false,
+                    readOnly = true,
+                    label = { Text("Profesor") }
+                )
+                DropdownMenu(
+                    expanded = expandedTeacher,
+                    onDismissRequest = { expandedTeacher = false },
+                ) {
+                    teachers.forEach { teacher ->
+                        DropdownMenuItem(
+                            text = {Text(teacher.teacherName)},
+                            onClick = {
+                                selectedTeacher = teacher
+                                expanded = false
+                            }
+                        )
+                    }
+                }
+            }
+
+            Spacer(Modifier.padding(vertical = 16.dp))
+            Button(
+                onClick = {
+                    if (selectedSubject != null ) {
+                        val newEvent = Event(
+                            title = title,
+                            description = description,
+                            start = "${startDate}T${String.format("%02d:%02d", startHour, startMinute)}-05:00",
+                            //start = start,
+                            // end = end,
+                            end ="${endDate}T${String.format("%02d:%02d", endHour, endMinute)}-05:00",
+                            teacher = teacher
+                        )
+                        eventViewModel.createEvent(
+                            newEvent,
+                            selectedSubject!!,
+                            selectedTeacher!!,
+                            selectedCategory!!
+                        )
+                        title = ""
+                        description = ""
+                        // end = ""
+                        endHour = 0
+                        endMinute = 0
+                        start = ""
+                    }
+
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Crear Evento")
+            }
         }
     }
+
+
 }
 
 @Preview(showBackground = true)
